d83f998544726ffbbbbc485f09b309d48a1ea6f9
omansh <omanshdhawan45@gmail.com>
2021-07-31 12:27:44 +0530

    final_Result

--- activity/command/organize.js
+++ activity/command/organize.js
@@ -12,10 +12,12 @@ function organize(src){
     if(src==undefined){
         src=process.cwd();
     }
-    let allEntities=fs.readdirSync(src);
-    let src2=path.join(src,"OrganisedFiles");
-    fs.mkdirSync(src2);
     
+    let src2=path.join(src,"OrganisedFiles");
+    if(fs.existsSync(src2)==false){
+        fs.mkdirSync(src2);
+    }
+    let allEntities=fs.readdirSync(src);
     for(let i=0;i<allEntities.length;i++){
         let fileName=allEntities[i];
         let fullPath=path.join(src,fileName);
@@ -78,6 +80,58 @@ function organize(src){
 
     return "organize command executed with path "+"\""+src+"\"";
 }
+
+// function organize(srcPath) {
+//     if (srcPath == undefined)
+//         srcPath = process.cwd();
+//     // console.log("organize implemneted", srcPath);
+//     // 1 create organized_files -> directory
+//     let organizedFilesPath = path.join(srcPath, "organized_files");
+//     if (fs.existsSync(organizedFilesPath) == false) {
+//         fs.mkdirSync(organizedFilesPath);
+//     }
+//     // 2 scan whole srcPath 
+//     let allTheFiles = fs.readdirSync(srcPath);
+//     // console.log(allTheFiles);
+//     // 3. extension check -> classify
+//     for (let i = 0; i < allTheFiles.length; i++) {
+//         let fullOriginalPath = path.join(srcPath, allTheFiles[i]);
+//         if (fs.lstatSync(fullOriginalPath).isFile() == true) {
+
+//             let folderName = checkextnTellFolder(allTheFiles[i]);
+//             copyFileTOdest(folderName, fullOriginalPath, srcPath);
+//         }
+//     }
+//     //  3. copy to that folder to which it belongs
+//     // folder
+//     // // file copy 
+//     // other
+//     // // file copy  
+// }
+// function copyFileTOdest(folderName, fullOriginalPath, srcPath) {
+//     let destFolderPath = path.join(srcPath, "organized_files",folderName);
+//     if (fs.existsSync(destFolderPath) == false) {
+//         fs.mkdirSync(destFolderPath);
+//     }
+//     let originalFileName = path.basename(fullOriginalPath);
+//     let destFilePath = path.join(destFolderPath, originalFileName)
+//     fs.copyFileSync(fullOriginalPath, destFilePath);
+//     console.log(originalFileName, "copeid to ", folderName);
+
+// }
+// function checkextnTellFolder(fileName) {
+//     let extName = path.extname(fileName);
+//     extName = extName.slice(1);
+//     for (let key in types) {
+//         for (let i = 0; i < types[key].length; i++) {
+//             if (types[key][i] == extName) {
+//                 return key;
+//             }
+//         }
+//     }
+//     return "others";
+// }
+
 
 module.exports={
     fxn:organize
